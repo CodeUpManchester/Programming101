7f6bd476766893a6bd7905353f7f7e25a72386bf
Drew Morgan <dangerworm@gmail.com>
2018-05-13 11:40:12 +0100

    Edited the logic for  second pattern generator

--- Programs/Turtle/patterns2.py
+++ Programs/Turtle/patterns2.py
@@ -1,28 +1,37 @@
 from turtle import *
 
-angleResult = input("How sharp are the star points? Angle (degrees): ")
-angle = 27 if angleResult == "" else int(angleResult)
+def drawStar(numberOfPoints, sideLength, doFill = False):
+    if numberOfPoints < 5:
+        angle = 360 // numberOfPoints
+    elif numberOfPoints > 4 and numberOfPoints < 7:
+        angle = 180 - (360 // numberOfPoints // 2)
+    else:
+        angle = 180 - (360 // numberOfPoints)
+
+    penup()
+    goto(-sideLength/2, 0)
+
+    hideturtle()
+    color("orange", "red")
+    speed(10)
+
+    if doFill:
+        begin_fill()
+
+    pendown()
+    for n in range(starPoints):
+        forward(sideLength)
+        right(angle)
+    penup()
+
+    if doFill:
+        end_fill()
+
+starPointsResult = input("How many points should the star have? ")
+starPoints = 20 if starPointsResult == "" else int(starPointsResult)
 
 sideLengthAnswer = input("How long are the sides? ")
 sideLength = 200 if sideLengthAnswer == "" else int(sideLengthAnswer)
 
-goto(-sideLength/2, 0)
-startX = position()[0]
-startY = position()[1]
+drawStar(starPoints, sideLength)
 
-hideturtle()
-color("orange", "red")
-speed(10)
-##begin_fill()
-
-pendown()
-while True:
-    forward(sideLength)
-    right(180 - angle)
-
-    currentX = position()[0]
-    currentY = position()[1]
-    if currentX == startX and currentY == startY:
-        break
-penup()
-##end_fill()
